0a88e8689e15f14ef3b360bef66a98bde440403e
Junior-22 <juniorfrancis2608@gmail.com>
2022-03-06 01:10:39 +0100

    Pokemon list
    If applied, this commit will have added the pokemon list

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-alert('Junior welcomes you');
-
-let favouriteSport = 'Football';
-document.write(favouriteSport);
+let pokemonList = [
+  {
+    name: 'Ivysaur',
+    height: 1,
+    wieght: 13,
+    types: ['grass', 'electric']
+  },
+  {
+    name: 'Sandslash',
+    height: 1,
+    weight: 29.5,
+    types: ['ground', 'rock']
+  },
+  {
+    name: 'Nidoqueen',
+    height: 1.3,
+    weight: 60,
+    types: ['bug', 'fairy']
+  },
+  {
+    name: 'Wigglytuff',
+    height: 1,
+    weight: 12,
+    types: ['dark', 'steel']
+  }
+];
