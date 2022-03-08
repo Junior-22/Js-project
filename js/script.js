564a7ad57516f9271273d53426c81d4fa7302873
Junior-22 <juniorfrancis2608@gmail.com>
2022-03-08 14:37:44 +0100

    create loop and write in DOM
    If applied, this commit will have created a loop and written in the DOM

--- js/script.js
+++ js/script.js
@@ -24,3 +24,18 @@ let pokemonList = [
     types: ['normal', 'fairy']
   }
 ];
+
+// loop iterates over the items in the pokemonList
+for (let i = 0; i < pokemonList.length; i++) {
+ console.log(pokemonList [i]);
+}
+
+// write in the DOM
+function newContent() {
+  document.open();
+  document.write("<h2>Ivysaur's height is 1</h2>");
+  document.write("<h2>Sandslash's height is 1</h2>");
+  document.write("<h2>Nidoqueen's height is 1.3</h2>");
+  document.write("<h2>Wigglytuff's height is 1</h2>");
+  document.close();
+}
