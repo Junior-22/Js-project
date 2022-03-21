5c953e7b6df3021f458553f43f996588142ed820
Junior-22 <juniorfrancis2608@gmail.com>
2022-03-21 21:59:01 +0100

    updated JS
    If applied, this commit will have added info for modals

--- js/script.js
+++ js/script.js
@@ -96,3 +96,62 @@ pokemonRepository.loadList().then(function() {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+let pokemonRepository = (function (){
+let modalContainer = document.querySelector('#modal-container');
+
+function showModal(title, text) {
+  // clear existing modal content
+  modalContainer.innerHTML = ' ';
+
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  // add new modal content
+  let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'close';
+  // Close modal using 'Close' button
+  closeButtonElement.addEventListener('click', hidemodal);
+
+  let titleElement = document.createElement('h1');
+  titleElement.innerText = title;
+
+  let contentElement = document.createElement('p');
+  contentElement.innerText = text;
+
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(contentElement);
+  modalContainer.appendChild(modal);
+
+  modalContainer.classList.add('is-visible');
+}
+
+// close modal
+function hideModal() {
+  let modalContainer = document.querySelector('#modal-container');
+  modalContainer.classList.remove('is-visible');
+}
+
+// CLose modal using ESC key
+window.addEventListener('keydown', (e) => {
+  let modalContainer = document.querySelector('#modal-container');
+  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    hideModal();
+  }
+});
+
+// CLose modal by clicking outside of modal
+modalContainer.addEventListener('click', (e) => {
+  let target = e.target;
+  if (target === modalContainer) {
+    hideModal();
+  }
+});
+
+
+document.querySelector('#show-modal').addEventListener('click', () => {
+  showModal();
+});
+})();
